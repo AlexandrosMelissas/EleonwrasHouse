c81f5ac4eedab7cf313fdfa7ba831e47d352332d
unknown <melissasalexandros@gmail.com>
2021-02-07 19:20:03 +0200

    upgraded to v2,switched to nodemailer and added amenities

--- functions/index.js
+++ functions/index.js
@@ -2,32 +2,40 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 admin.initializeApp()
 const db = admin.firestore()
-const sgmail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer')
 
-const API_KEY = functions.config().sendgrid.key
-const TEMPLATE_ID = functions.config().sendgrid.template
+// const API_KEY = functions.config().sendgrid.key
+// const TEMPLATE_ID = functions.config().sendgrid.template
 
-sgmail.setApiKey(API_KEY)
+// sgmail.setApiKey(API_KEY)
 
 
-exports.contactform = functions.firestore.document('contact/{contactId}').onCreate(async (change,context) => {
+exports.contactform = functions.firestore.document('contact/{contactId}').onCreate(async (change, context) => {
 
     const contactSnap = await db.collection('contact').doc(context.params.contactId).get()
 
     const contact = contactSnap.data()
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: functions.config().contact.email,
+            pass: functions.config().contact.password
+        },
+    });
 
-    const msg = {
-        to: "mrgatossssssssss@gmail.com",
-        from: contact.email,
-        templateId : TEMPLATE_ID,
-        dynamic_template_data : {
-            full_name : contact.full_name,
-            email : contact.email,
-            message: contact.message
-        }
-    }
+    const mailOptions = {
+        from: functions.config().contact.email,
+        to: 'eleonwrashouse@gmail.com',
+        subject: 'New message from Eleonwras house',
+        text: `
+        Full Name: ${contact.full_name} \n
+        Email: ${contact.email} \n
+        Message: ${contact.message} \n
+        Date: ${contact.message_date}
+        `
+    };
 
-
-    return sgmail.send(msg)
+    console.log('hello')
+    transporter.sendMail(mailOptions).catch(e => { console.log(e) })
 
 })
